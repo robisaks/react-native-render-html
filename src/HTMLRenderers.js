dda6fab0c6537cb701e314ac1e6c3ab55ea98787
Max Maes <max@livewallgroup.com>
2019-04-29 13:32:28 +0200

    Use react-native webview

--- src/HTMLRenderers.js
+++ src/HTMLRenderers.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { TouchableOpacity, Text, View, WebView, Platform } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { TouchableOpacity, Text, View, Platform } from 'react-native';
 import { _constructStyles, _getElementClassStyles } from './HTMLStyles';
 import HTMLImage from './HTMLImage';
 
